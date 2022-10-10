85c74a5cfd8a2744085dda92546a9d6909c9b6d9
Vinicius <vinicius_henrique96@hotmail.com>
2022-10-10 12:04:13 -0300

    :recycle: refactor: Refatoração no JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,35 @@ const testimoniesBox = document.querySelectorAll(".testimonial__box");
 const btnNext = document.querySelector(".btn__next");
 const btnPrev = document.querySelector(".btn__prev");
 
-const transition = function () {
-  testimoniesBox.forEach((testi) => {
-    testi.classList.contains("visible")
-      ? testi.classList.remove("visible")
-      : testi.classList.add("visible");
-  });
+let contSlide = 0;
+let indexTesti = testimoniesBox.length - 1;
+
+const returnSlide = function (index) {
+  testimoniesBox[contSlide].style.opacity = 0;
+  contSlide = index;
+  testimoniesBox[contSlide].style.opacity = 1;
 };
 
-btnNext.addEventListener("click", transition);
-btnPrev.addEventListener("click", transition);
+const nextSlide = function () {
+  if (contSlide !== indexTesti) {
+    testimoniesBox[contSlide + 1].previousElementSibling.style.opacity = 0;
+    testimoniesBox[contSlide + 1].style.opacity = 1;
+
+    contSlide += 1;
+  } else {
+    returnSlide(0);
+  }
+};
+
+const prevSlide = function () {
+  if (contSlide <= indexTesti && contSlide != 0) {
+    testimoniesBox[contSlide - 1].style.opacity = 1;
+    testimoniesBox[contSlide].style.opacity = 0;
+    contSlide -= 1;
+  } else {
+    returnSlide(indexTesti);
+  }
+};
+
+btnNext.addEventListener("click", nextSlide);
+btnPrev.addEventListener("click", prevSlide);
