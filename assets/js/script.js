6cb8c1fa6c199548605e43c384eeffc9167bfa51
Vinicius <vinicius_henrique96@hotmail.com>
2022-10-13 10:24:42 -0300

    :sparkles: feat: Implementando controle do slide com keydown

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,50 @@
 
 // Selecting Elements
 
-const testimoniesBox = document.querySelectorAll(".testimonial__box");
-const btnNext = document.querySelector(".btn__next");
-const btnPrev = document.querySelector(".btn__prev");
+const slider = function () {
+  const testimoniesBox = document.querySelectorAll(".testimonial__box");
+  const btnNext = document.querySelector(".btn__next");
+  const btnPrev = document.querySelector(".btn__prev");
 
-let contSlide = 0;
-let indexTesti = testimoniesBox.length - 1;
+  let contSlide = 0;
+  let indexTesti = testimoniesBox.length - 1;
 
-const returnSlide = function (index) {
-  testimoniesBox[contSlide].style.opacity = 0;
-  contSlide = index;
-  testimoniesBox[contSlide].style.opacity = 1;
-};
-
-// Logic for next
-const nextSlide = function () {
-  if (contSlide !== indexTesti) {
-    testimoniesBox[contSlide + 1].previousElementSibling.style.opacity = 0;
-    testimoniesBox[contSlide + 1].style.opacity = 1;
-    contSlide += 1;
-  } else {
-    returnSlide(0);
-  }
-};
-
-// Logic to go previous
-const prevSlide = function () {
-  if (contSlide <= indexTesti && contSlide !== 0) {
-    testimoniesBox[contSlide - 1].style.opacity = 1;
+  const returnSlide = function (index) {
     testimoniesBox[contSlide].style.opacity = 0;
-    contSlide -= 1;
-  } else {
-    returnSlide(indexTesti);
-  }
+    contSlide = index;
+    testimoniesBox[contSlide].style.opacity = 1;
+  };
+
+  // Logic for next
+  const nextSlide = function () {
+    if (contSlide !== indexTesti) {
+      testimoniesBox[contSlide + 1].previousElementSibling.style.opacity = 0;
+      testimoniesBox[contSlide + 1].style.opacity = 1;
+      contSlide += 1;
+    } else {
+      returnSlide(0);
+    }
+  };
+
+  // Logic to go previous
+  const prevSlide = function () {
+    if (contSlide <= indexTesti && contSlide !== 0) {
+      testimoniesBox[contSlide - 1].style.opacity = 1;
+      testimoniesBox[contSlide].style.opacity = 0;
+      contSlide -= 1;
+    } else {
+      returnSlide(indexTesti);
+    }
+  };
+
+  btnNext.addEventListener("click", nextSlide);
+  btnPrev.addEventListener("click", prevSlide);
+
+  // Slide with keydown
+  document.addEventListener("keydown", function (e) {
+    e.key === "ArrowRight" && nextSlide();
+    e.key === "ArrowLeft" && prevSlide();
+  });
 };
 
-btnNext.addEventListener("click", nextSlide);
-btnPrev.addEventListener("click", prevSlide);
+slider();
